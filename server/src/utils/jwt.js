1865de06cf28cc0dcf196bdd471b008a2453de13
Prashishgt <prashish24@gmail.com>
2023-11-17 12:14:17 +0545

    Initilized functions for creating and verifying jwt token

---
+++ server/src/utils/jwt.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+
+const createToken = (payload) => {
+  try {
+    const createdToken = jwt.sign(
+      {
+        data: payload,
+      },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: process.env.JWT_EXPIRY,
+      }
+    );
+
+    return createToken;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const verifyJWT = (token) => {
+  try {
+    const verifiedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    return verifiedToken;
+  } catch (error) {
+    throw new Error("Token is invalid");
+  }
+};
+
+export { createToken, verifyJWT };
